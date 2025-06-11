cc9dc2871ca728a373d91b7ea1a317314bb80d57
matej2314 <mateo2314@gmail.com>
2025-06-11 15:05:47 +0200

    fixed nodemailer.ts file

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,15 +1,13 @@
 import nodemailer, { type Transporter } from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
-import { emailData } from '../emaildata';
-
 export const transporter: Transporter<SMTPTransport.SentMessageInfo> = nodemailer.createTransport({
 	host: process.env.MAIL_HOST,
 	port: 465,
 	secure: true,
 	auth: {
-		user: emailData.login,
-		pass: emailData.pass,
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASS,
 	},
 } as SMTPTransport.Options);
 
