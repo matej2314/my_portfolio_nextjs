d5f4211cee9638e494807a4fa4600f4447e9356c
matej2314 <mateo2314@gmail.com>
2025-06-11 23:22:22 +0200

    simple change

--- app/home/blog/page.tsx
+++ app/home/blog/page.tsx
@@ -4,7 +4,7 @@ export default async function BlogPage() {
     return (
         <div className="relative flex-1 flex flex-col">
             <div className="w-full flex flex-col">
-                <p>blog page</p>
+                <p className="text-slate-200">blog page</p>
             </div>
         </div>
     )
