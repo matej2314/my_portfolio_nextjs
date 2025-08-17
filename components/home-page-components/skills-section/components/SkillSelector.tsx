4d36edc055b9f7989eb2fe79ced260b6c57f1bf7
mateo2314 <mateo2314@gmail.com>
2025-08-17 23:10:07 +0200

    Change layout from flex to grid in SkillSelector component.

--- components/home-page-components/skills-section/components/SkillSelector.tsx
+++ components/home-page-components/skills-section/components/SkillSelector.tsx
@@ -6,31 +6,15 @@ import { Button } from '@/components/ui/button';
 import { type SkillSelectorProps } from '@/types/skillSelectorTypes';
 
 export default function SkillSelector({ clickAction, selectedCategory, categories }: SkillSelectorProps) {
-
-    return (
-        <motion.ul
-            initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5, ease: easeOut }}
-            viewport={{ amount: 0.4, once: false }}
-            className='w-full h-fit flex justify-center gap-2 min-[450px]:gap-3 min-[515px]:gap-6 min-[580px]:gap-7'
-        >
-            {categories.map((category, index) => (
-                <li
-                    key={`${category.name}-${index}`}
-                >
-                    <Button
-                        onClick={() => clickAction(category.name as string)}
-                        className={`w-fit h-fit ${selectedCategory === category.name
-                            ? 'bg-yellow-400'
-                            : 'bg-yellow-200'
-                            } text-slate-950 hover:bg-yellow-300 hover:shadow-md text-[0.75rem] font-semibold sm:tracking-wide md:font-normal md:text-base hover:scale-110`}
-                    >
-                        {category.label}
-                    </Button>
-                </li>
-            ))}
-        </motion.ul>
-    )
-
+	return (
+		<motion.ul initial={{ opacity: 0, scale: 0.8 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, ease: easeOut }} viewport={{ amount: 0.4, once: false }} className='w-full h-fit grid grid-cols-3 gap-2 min-[450px]:grid-cols-3 min-[515px]:grid-cols-4 min-[580px]:grid-cols-3 text-center'>
+			{categories.map((category, index) => (
+				<li key={`${category.name}-${index}`}>
+					<Button onClick={() => clickAction(category.name as string)} className={`w-fit h-fit ${selectedCategory === category.name ? 'bg-yellow-400' : 'bg-yellow-200'} text-slate-950 hover:bg-yellow-300 hover:shadow-md text-[0.75rem] font-semibold sm:tracking-wide md:font-normal md:text-base hover:scale-110`}>
+						{category.label}
+					</Button>
+				</li>
+			))}
+		</motion.ul>
+	);
 }
