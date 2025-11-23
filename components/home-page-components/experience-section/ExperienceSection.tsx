c4130ad3a069b10ba9b5906809ac09e28fa244c3
matej2314 <mateo2314@gmail.com>
2025-11-23 16:07:34 +0100

    fix experience section issues

--- components/home-page-components/experience-section/ExperienceSection.tsx
+++ components/home-page-components/experience-section/ExperienceSection.tsx
@@ -7,9 +7,23 @@ export default async function ExperienceSection({experiences}: {experiences: Get
         return <p>Failed to fetch experiences</p>
     }
 
+    const experienceData = experiences.experiences;
+
     return (
         <section id="experienceSection" className="w-full h-fit flex flex-col text-slate-200 snap-center">
             <span className="text-4xl text-green-400 ml-2">Experience &#123;</span>
+            <div>
+                {experienceData.map(experience => (
+                    <div key={experience.uuid}>
+                        <h3 className="text-2xl text-green-400">{experience.employer}</h3>
+                        <p className="text-lg text-white">{experience.position}</p>
+                        <p className="text-lg text-white">{experience.hourly_rate}</p>
+                        <p className="text-lg text-white">{new Date(experience.employed_since).toLocaleDateString()}</p>
+                        <p className="text-lg text-white">{new Date(experience.employed_to).toLocaleDateString()}</p>
+                    </div>
+                ))}
+            </div>
+            <span className='text-4xl text-green-400 w-full ml-2 mt-[13rem] lg:mt-[4rem]'>&#125;</span>
         </section>
     )
 }
