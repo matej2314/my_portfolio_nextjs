9db38906f028d9c3d98606b594faaf598e2f8393
matej2314 <mateo2314@gmail.com>
2025-11-29 21:46:59 +0100

    fix data type errors to build

--- components/control-panel-components/forms/ExperienceForm.tsx
+++ components/control-panel-components/forms/ExperienceForm.tsx
@@ -5,19 +5,19 @@
 // import FormTitle from "./components/FormTitle";
 // import { getSubmitFunction } from "@/lib/utils/getSubmitFunction";
 // // import { saveExperience, updateExperience } from "@/actions/experience";
-import { type ExperienceFormProps } from '@/types/forms/experience-form';
+// import { type ExperienceFormProps } from '@/types/forms/experience-form';
 
 
-export default function ExperienceForm({ experienceData, mode = 'create' }: ExperienceFormProps) {
+// export default function ExperienceForm({mode = 'create' }: ExperienceFormProps) {
 
-// const submitFunction = getSubmitFunction({
-//     create: saveExperience,
-//     edit: updateExperience
-// }, mode);
+// // const submitFunction = getSubmitFunction({
+// //     create: saveExperience,
+// //     edit: updateExperience
+// // }, mode);
 
 
 
-    return (
-        <main className="w-full h-full flex flex-col justify-center items-center text-slate-200"></main>
-    )
-}
+//     return (
+//         <main className="w-full h-full flex flex-col justify-center items-center text-slate-200"></main>
+//     )
+// }
