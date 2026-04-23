ba24cad99f764c2d60f1af23aedc0a8ac3e91b0c
mateo2314 <mateo2314@gmail.com>
2026-04-23 21:43:07 +0200

    .

--- types/utils/get-delete-function.ts
+++ types/utils/get-delete-function.ts
@@ -1,5 +1,7 @@
 import { type ReturnedType } from '@/types/actionsTypes/actionsTypes';
 
+// get delete function for delete any data from the database
+
 export type DeleteFunction = (prevState: ReturnedType, formData: FormData) => Promise<ReturnedType>;
 
 export type DataType = 'course' | 'project' | 'skill' | 'blogPost' | 'experience';
